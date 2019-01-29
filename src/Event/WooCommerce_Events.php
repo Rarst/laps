8be22411f7be2a28dd3a85343e973fe85186de56
Andrey Savchenko <contact@rarst.net>
2019-01-29 16:34:08 +0200

    Added WooCommerce loop item event.

--- src/Event/WooCommerce_Events.php
+++ src/Event/WooCommerce_Events.php
@@ -16,6 +16,7 @@ class WooCommerce_Events implements Hook_Event_Config_Interface {
 		return class_exists( 'WooCommerce' ) ? [
 			[ 'WooCommerce Init', 'plugin', 'before_woocommerce_init', 'woocommerce_init' ],
 			[ 'WooCommerce Shop Loop', 'plugin', 'woocommerce_before_shop_loop', 'woocommerce_after_shop_loop' ],
+			[ 'WooCommerce Shop Item', 'plugin', 'woocommerce_before_shop_loop_item', 'woocommerce_after_shop_loop_item' ],
 			[ 'WooCommerce Single Product', 'plugin', 'woocommerce_before_single_product', 'woocommerce_after_single_product' ],
 			[ 'WooCommerce Cart', 'plugin', 'woocommerce_before_cart', 'woocommerce_after_cart' ],
 			[ 'WooCommerce Checkout', 'plugin', 'woocommerce_before_checkout_form', 'woocommerce_after_checkout_form' ],
